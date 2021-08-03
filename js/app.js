014f7524b128b0f0f377c8a6e83596d1eba0bb7a
huntercront <38252870+huntercront@users.noreply.github.com>
2021-08-03 16:17:01 +0300

    add strict

---
+++ js/app.js
@@ -0,0 +1,149 @@
+WebFontConfig = {
+    google: { families: ['Inter:400,600,700,900'] }
+};
+
+(function(d) {
+    var wf = d.createElement('script'),
+        s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
+
+
+//loader function
+var Loader = function() {}
+Loader.prototype = {
+    require: function(scripts, callback) {
+        this.loadCount = 0;
+        this.totalRequired = scripts.length;
+        this.callback = callback;
+
+        for (var i = 0; i < scripts.length; i++) {
+            this.writeScript(scripts[i]);
+        }
+    },
+    loaded: function(evt) {
+        this.loadCount++;
+
+        if (this.loadCount == this.totalRequired && typeof this.callback == 'function') this.callback.call();
+    },
+    writeScript: function(src) {
+        var self = this;
+        var s = document.createElement('script');
+        s.type = "text/javascript";
+        s.defer = true;
+        s.src = src;
+        s.addEventListener('load', function(e) { self.loaded(e); }, false);
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(s);
+    }
+}
+
+
+var l = new Loader();
+l.require([
+        "../js/slider.js",
+    ],
+    function() {
+
+        let mySiema = new Siema({
+            selector: '.slider-content',
+            perPage: 2
+        });
+        let prev = document.querySelector('.prev');
+        let next = document.querySelector('.next');
+
+        prev.addEventListener('click', () => mySiema.prev());
+        next.addEventListener('click', () => mySiema.next());
+
+
+    });
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let slideUp = (target, duration = 500) => {
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.boxSizing = 'border-box';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout(() => {
+            target.style.display = 'none';
+            target.style.removeProperty('height');
+            target.style.removeProperty('padding-top');
+            target.style.removeProperty('padding-bottom');
+            target.style.removeProperty('margin-top');
+            target.style.removeProperty('margin-bottom');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+            //alert("!");
+        }, duration);
+    }
+
+    let slideDown = (target, duration = 500) => {
+        target.style.removeProperty('display');
+        let display = window.getComputedStyle(target).display;
+        if (display === 'none')
+            display = 'block';
+        target.style.display = display;
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.boxSizing = 'border-box';
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout(() => {
+            target.style.removeProperty('height');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+        }, duration);
+    }
+    var slideToggle = (target, duration = 500) => {
+        if (window.getComputedStyle(target).display === 'none') {
+            return slideDown(target, duration);
+        } else {
+            return slideUp(target, duration);
+        }
+    }
+
+
+    let books = document.querySelectorAll('.leaning-button')
+    books.forEach(function(book) {
+        book.addEventListener('click', function(event) {
+            if (event.target.closest('.leaning-el').classList.contains('active-el')) {
+                slideUp(event.target.closest('.leaning-el').querySelector('.hidden-list'), 300)
+                event.target.closest('.leaning-el').classList.remove('active-el')
+            } else {
+                if (document.querySelector('.active-el')) {
+                    slideToggle(document.querySelector('.active-el .hidden-list'), 300)
+                    document.querySelector('.active-el').classList.toggle('active-el')
+                }
+                slideDown(event.target.closest('.leaning-el').querySelector('.hidden-list'), 300)
+                event.target.closest('.leaning-el').classList.add('active-el')
+
+            }
+
+        })
+    })
+
+
+})
